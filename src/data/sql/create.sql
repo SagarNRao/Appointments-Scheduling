a0b3a37e0a7830f2af89070ffc788025387c2958
SagarNRao <sagarnrao154@gmail.com>
2024-11-02 13:58:12 +0530

    first commit

--- src/data/sql/create.sql
+++ src/data/sql/create.sql
@@ -1,6 +1,6 @@
-DROP TABLE public.users;
+DROP TABLE IF EXISTS "public"."users";
 
-CREATE TABLE public.users
+CREATE TABLE "public"."users"
 (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
@@ -12,5 +12,5 @@ CREATE TABLE public.users
     CONSTRAINT update_at_on_change CHECK (updated_at >= created_at)
 ); 
 
-INSERT INTO public.users (name, email, password) 
+INSERT INTO "public"."users" (name, email, password) 
 VALUES ('Example', 'example@example.com', 'hashed_password');
